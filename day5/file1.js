4fd2697b1867084f4ba15f08d7dbabf6360020c9
imran <ipathan809@gmail.com>
2023-07-05 15:36:01 +0530

    9

--- day5/file1.js
+++ day5/file1.js
@@ -2,6 +2,6 @@ console.log("heye this is my first git");
 console.log("this is b2 branch's first git");
 console.log("hey this is my second git");
 console.log("git hub started successfully");
-
+console.log("bad boy");
 //this is b1 for change
 hello 
