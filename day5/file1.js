496d1c6492c02e6e51ca28f3569f1331c3ea806f
Irfan-97 <irfanmalvat11@gmail.com>
2023-07-05 15:02:21 +0530

    github updated

--- day5/file1.js
+++ day5/file1.js
@@ -1,6 +1,7 @@
 console.log("heye this is my first git");
 console.log("this is b2 branch's first git");
 console.log("hey this is my second git");
+console.log("git hub started successfully");
 
 //this is b1 for change
 hello 
